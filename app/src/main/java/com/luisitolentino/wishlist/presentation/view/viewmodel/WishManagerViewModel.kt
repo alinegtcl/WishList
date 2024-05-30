87c631fd6e0775ce07bd2a374c287d8efba0904a
aline <linegcruz@gmail.com>
2024-05-29 23:23:56 -0300

    feat(search_wishes): integration of wish search with the database.

--- app/src/main/java/com/luisitolentino/wishlist/presentation/view/viewmodel/WishManagerViewModel.kt
+++ app/src/main/java/com/luisitolentino/wishlist/presentation/view/viewmodel/WishManagerViewModel.kt
@@ -3,25 +3,32 @@ package com.luisitolentino.wishlist.presentation.view.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.luisitolentino.wishlist.domain.usecase.WishManagerUseCase
-import kotlinx.coroutines.Dispatchers
+import com.luisitolentino.wishlist.domain.utils.flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class WishManagerViewModel(private val useCase: WishManagerUseCase) : ViewModel() {
 
     private val _stateList = MutableStateFlow<WishState>(WishState.HideLoading)
     val stateList: StateFlow<WishState> = _stateList
 
-    fun getAllWishes() {
+    fun getAllWishes(orderByName: Boolean = true) {
         viewModelScope.launch {
             _stateList.value = WishState.ShowLoading
-            withContext(Dispatchers.IO) {
-                Thread.sleep(1000)
-            }
+            val response = useCase.getAllWishesByName(orderByName)
             _stateList.value = WishState.HideLoading
-            _stateList.value = WishState.EmptyState
+            response.flow(
+                { wishes ->
+                    if (wishes.isNotEmpty())
+                        _stateList.value = WishState.SearchAllSuccess(wishes)
+                    else
+                        _stateList.value = WishState.EmptyState
+                },
+                {
+                    _stateList.value = WishState.Failure(it)
+                }
+            )
         }
     }
 
